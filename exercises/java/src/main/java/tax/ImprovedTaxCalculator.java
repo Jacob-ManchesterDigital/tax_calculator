8accc2661d3902653a6b89e4c2360ef96703bfbb
Jacob Walsh <walsh+manchesterdigital@posteo.net>
2021-05-17 12:20:45 +0100

    Refactor ImprovedTaxCalculator

--- exercises/java/src/main/java/tax/ImprovedTaxCalculator.java
+++ exercises/java/src/main/java/tax/ImprovedTaxCalculator.java
@@ -10,8 +10,19 @@ public class ImprovedTaxCalculator extends TaxCalculator {
     int calculateTax(Vehicle vehicle) {
         int taxAmount = 0;
         int emissions = vehicle.getCo2Emissions();
+        int[][] taxBoundaries = new int[][]
+                {{256,2070},{226,1760},{191,1240},{171,830},{151,515},
+                        {131,205},{111,165},{101,145},{91,125},{76,105},
+                        {51,25},{1,10}};
 
-        if (emissions > 255){
+        for (int[] boundary:taxBoundaries){
+            if (emissions >= boundary[0]){
+                taxAmount = boundary[1];
+                break;
+            }
+        }
+
+        /*if (emissions > 255){
             taxAmount = 2070;
         }
         else if (emissions >= 226){
@@ -46,7 +57,7 @@ public class ImprovedTaxCalculator extends TaxCalculator {
         }
         else if (emissions >= 1){
             taxAmount = 10;
-        }
+        }*/
 
         return taxAmount;
     }
